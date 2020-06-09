5615fcf1c7a9a74aa4e7974e38ebc5d055cf59dc
Laureni Wilkinson <las348@nyu.edu>
2020-06-09 14:47:24 -0400

    query update

--- config/orm.js
+++ config/orm.js
@@ -1,35 +1,38 @@
 const connection = require("./connection");
 
 const orm = {
-selectAll: (selectQuery) => {
-    connection.query(`SELECT * FROM burgers;`, function(err, result){
-        if (err) {
-            return res.status(500).end();
-          }
-        console.log(result)
-        selectQuery(result)
-    })
-   },
+    selectAll: (res) => {
+        connection.query(`SELECT * FROM burgers;`, function (err, data) {
+            if (err) {
+                return res.status(500).end();
+            }
+            console.log(data)
+            res.json({ burgers: data });
+        });
+    },
 
-   insertOne: (selectQuery) => {
-    connection.query(`INSERT INTO burgers (burger_name) VALUES (?)`, function(err, result){
-        if (err) {
-            return res.status(500).end();
-          }
-        console.log(result)
-        selectQuery(result)
-    })
-   },
+    insertOne: (res) => {
+        connection.query(`INSERT INTO burgers (burger_name) VALUES (?)`, [req.body.burger_name], function (err, result) {
+            if (err) {
+                return res.status(500).end();
+            }
+            console.log({ id: result.insertId});
+            res.json({ id: result.insertId });
+        });
+    },
 
-   updateOne: (selectQuery) => {
-    connection.query(`UPDATE burgers SET burger_name = ? WHERE id = ?`, function(err, result){
-        if (err) {
-            return res.status(500).end();
-          }
-        console.log(result)
-        selectQuery(result)
-    })
-   }
-}
+    updateOne: (res) => {
+        connection.query(`UPDATE burgers SET burger_name = ? WHERE id = ?`, [req.body.burger_name, req.params.id], function (err, data) {
+            if (err) {
+                return res.status(500).end();
+            }
+            else if (result.changedRows === 0) {
+                return res.status(404).end();
+              }
+
+            res.status(200).end();
+        });
+    }
+};
 
 module.exports = orm;
