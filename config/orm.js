d036e11485f781a9690e932df8de6bba56ebbd75
Laureni Wilkinson <las348@nyu.edu>
2020-06-09 14:28:22 -0400

    initial orm

--- config/orm.js
+++ config/orm.js
@@ -1,13 +1,35 @@
 const connection = require("./connection");
 
-const orm = connection.getConnection(function (err, connection) {
-    function selectAll() 
+const orm = {
+selectAll: (selectQuery) => {
+    connection.query(`SELECT * FROM burgers;`, function(err, result){
+        if (err) {
+            return res.status(500).end();
+          }
+        console.log(result)
+        selectQuery(result)
+    })
+   },
 
-    function insertOne()
+   insertOne: (selectQuery) => {
+    connection.query(`INSERT INTO burgers (burger_name) VALUES (?)`, function(err, result){
+        if (err) {
+            return res.status(500).end();
+          }
+        console.log(result)
+        selectQuery(result)
+    })
+   },
 
-    function updateOne()
-});
-
-
+   updateOne: (selectQuery) => {
+    connection.query(`UPDATE burgers SET burger_name = ? WHERE id = ?`, function(err, result){
+        if (err) {
+            return res.status(500).end();
+          }
+        console.log(result)
+        selectQuery(result)
+    })
+   }
+}
 
 module.exports = orm;
