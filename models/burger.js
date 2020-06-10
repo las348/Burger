a8448a23d747078a845733c5ae5c46e452bc7df8
Laureni Wilkinson <las348@nyu.edu>
2020-06-10 12:39:03 -0400

    model update of object

--- models/burger.js
+++ models/burger.js
@@ -1,10 +1,29 @@
+//MODEL
 const orm = require("../config/orm");
-const express = require("express");
-const app = express();
 
-// Also inside `burger.js`, create the code that will call the ORM functions using burger specific input for the ORM.
-orm.selectAll("","");
-orm.insertOne("","");
-orm.updateOne("","");
-
-module.exports = ;
+var burger = {
+    selectAll: function(cb) {
+      orm.selectAll("burgers", function(res) {
+        cb(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    insertOne: function(cols, vals, cb) {
+      orm.insertOne("burgers", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    updateOne: function(objColVals, condition, cb) {
+      orm.updateOne("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
+    },
+    delete: function(condition, cb) {
+      orm.delete("burgers", condition, function(res) {
+        cb(res);
+      });
+    }
+  };
+  
+  // Export the database functions for the controller
+  module.exports = burger;
