698ace1d52b602977349c75963c43accb383cf68
Laureni Wilkinson <las348@nyu.edu>
2020-06-15 19:54:33 -0400

    heroku

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,9 @@ CREATE DATABASE burgers_db;
 use burgers_db;
 
 create table burgers (
-id int NOT NULL auto_increment primary key,
+id INT AUTO_INCREMENT NOT NULL,
 burger_name varchar(50) NOT NULL,
-devoured boolean DEFAULT false
+devoured boolean DEFAULT false,
+createdAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY(id)
 );
